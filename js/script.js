088528fe656ba67b37d4a3b835f253312ad16753
paco-stack-crimson <francisco@laken.xyz>
2025-09-24 20:54:04 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,11 +13,9 @@ const svg = d3.select("#chart")
 
 // Use Promise.all to load both data files at the same time
 Promise.all([
-  d3.csv("incidentes.csv"),
+  d3.csv("Incidentes.csv"),
   d3.json("datos.json")
 ]).then(function(files) {
-
-  // ... rest of the script
 
   const trafficData = files[0];
   const intersectionsData = files[1].intersections;
